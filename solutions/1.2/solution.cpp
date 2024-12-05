399fafcff5fd610e53ba4ae62f414c575f76e1c7
Cody J <codywj777@gmail.com>
2024-12-04 22:30:33 -0800

    day one done in cpp

--- solutions/1.2/solution.cpp
+++ solutions/1.2/solution.cpp
@@ -1,35 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
     string line;
-
     vector<int> left;
     vector<int> right;
+    unordered_map<int, int> counts;
 
-    while(getline(cin, line)) {
-        // stringstream ss(line);
+    while (getline(cin, line)) {
+         // stringstream ss(line);
         stringstream ss = stringstream(line);
-
         int first, second;
-
         // read stringstream out to first and second
         ss >> first >> second;
-
+        counts[second]++;
         left.push_back(first);
         right.push_back(second);
     }
 
-    sort(left.begin(), left.end());
-    sort(right.begin(), right.end());
-
     int s = 0;
-    cout << (right[0]-left[0]) << endl;
-
     for (int i = 0; i < left.size(); i++) {
-        s += abs(left[i]-right[i]);
+        if (counts.find(left[i]) != counts.end()) {
+            s += left[i]*counts[left[i]];
+        }
     }
-    cout << s << "\n";
-    return s;
+    cout << s << endl;
 }
